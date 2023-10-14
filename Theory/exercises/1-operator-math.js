6683d3ed48e9cc1772acefcde9c62606ca2051aa
ratarca <ratarca.rtc@gmail.com>
2023-10-14 12:41:49 -0300

    update:exercises

---
+++ Theory/exercises/1-operator-math.js
@@ -0,0 +1,11 @@
+// 1 - Create a 
+let initial_position = 10;
+let initial_velocity = 25;
+let aceleration = 2;
+let time = 10;
+
+let position = initial_position + initial_velocity*time + (aceleration * Math.pow(time,2))/2
+console.log(`${position}`)
+
+// Calculator
+
