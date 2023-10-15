de124c71cea787e0ac18c6ca6e04d598afeb9851
ratarca <ratarca.rtc@gmail.com>
2023-10-15 15:42:52 -0300

    update: 01 module

--- Theory/01-basic/2-loops.js
+++ Theory/01-basic/2-loops.js
@@ -1,1 +1,13 @@
-// Control Flow : Loops
+/* Basic loops : for and while */
+
+// for loop
+for (let counter = 0; counter <= 10; counter++){
+    console.log(counter)
+}
+
+// While
+let age = 18;
+while (age <= 70){
+    console.log(`Working with ${age} years old...`)
+    age++
+}
