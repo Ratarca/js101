a7ba8cf5c36d8ac0a3892e1337756a79c3a39224
ratarca <ratarca.rtc@gmail.com>
2023-10-14 23:35:20 -0300

    upadate 00 module

---
+++ Theory/00-variables/1-types.js
@@ -0,0 +1,23 @@
+// basic types
+
+let myName = `some name`;
+let myAge = 17;
+let myIncome = 17.45;
+let booleanVariable = true;
+let nullVariable = null;
+let someVariable
+
+
+// string
+console.log(myName, typeof(myName))
+
+// Numeric
+console.log(myAge, typeof(myAge))
+console.log(myIncome, typeof(myIncome))
+
+// Boolean
+console.log(booleanVariable, typeof(booleanVariable))
+
+// Empty Varibles
+console.log(nullVariable, typeof(nullVariable))
+console.log(someVariable, typeof(someVariable))
