790e65049ea4d7794b27e71d206ef9aef1833ca3
ratarca <ratarca.rtc@gmail.com>
2023-10-14 23:19:25 -0300

    delete all

--- Theory/04-Functions/0_declaration.js
+++
@@ -1,7 +0,0 @@
-// Declaration
-function cacValue(investiments, new_clients){
-    cac = investiments/new_clients
-    return `Your cac is ${cac}`
-}
-
-console.log( cacValue(100,10) )
--- Theory/04-Functions/2_arrow.js
+++
@@ -1,5 +0,0 @@
-const SumNumbers = function(a,b){
-    return a+b;
-};
-
-console.log(SumNumbers(10,5));
