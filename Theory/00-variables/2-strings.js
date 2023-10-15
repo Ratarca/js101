33a9bf2937116a2f7ae6396308ccf24d54eb43a3
ratarca <ratarca.rtc@gmail.com>
2023-10-14 23:41:48 -0300

    update: 00 module

---
+++ Theory/00-variables/2-strings.js
@@ -0,0 +1,13 @@
+/* 
+    String and some methods
+    - 1: lenght
+    - 2: concat
+    - 3: substring
+    - 4: indexOf
+    - 5: replace
+    - 6: split 
+    - 7: concat
+    - 8: slice
+    - 9: trim
+    -10: toUpperCase and toLowerCase
+*/
---
+++ Theory/00-variables/3-numeric.js
@@ -0,0 +1,12 @@
+/* 
+    Numeric and some methods
+    - 1: parseInt and parseFloat
+    - 2: Number
+    - 3: toFixed()
+    - 4: Math.abs()
+    - 5: Math.round()
+    - 6: Math.floor() or Math.ceil()
+    - 7: Math.max() or Math.min() 
+    - 8: Math.random()
+    - 9: Math.pow()
+*/
