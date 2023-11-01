b73921efad86f396421d978b6870880142ef7e3c
ratarca <ratarca.rtc@gmail.com>
2023-11-01 19:40:01 -0300

    update

---
+++ 00-Fundamentals/01-operators/01-comparsion.js
@@ -0,0 +1,35 @@
+/*
+Theory
+> , >=
+< , <=
+==, ===
+!=
+
+*/
+
+
+let eggs_price = 12;
+let meat_price = 32;
+let chicken_price = 12;
+let chocolate = '12';
+
+console.log(`Comparsion eggs and meat   
+        equal: ${eggs_price == meat_price}
+        diffent: ${eggs_price != meat_price}
+        great:${eggs_price > meat_price}
+        less: ${eggs_price < meat_price}
+`);
+
+
+console.log(`Comparsion eggs and chicken   
+        great:${eggs_price > chicken_price}
+        great equal: ${eggs_price >= chicken_price}
+        less: ${eggs_price < chicken_price}
+        less  equal: ${eggs_price <= chicken_price}
+`);
+
+
+// VERIFY EGGS AND CHOCOLATE!
+
+// Equal restrict : How the compiler know when use == and ===
+console.log('Restriction: ', chicken_price==chocolate, chicken_price===chocolate );
---
+++ 00-Fundamentals/01-operators/03-ternary.js
@@ -0,0 +1,2 @@
+let result = (5 > 3) ? "Yes" : "No"; // result will be "Yes"
+console.log(result)
